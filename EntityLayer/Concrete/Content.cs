4e2aed43982fbf169587b982af2d37636b48247f
Songül YAMAN BAYIK <snglymn@gmail.com>
2022-10-20 19:15:20 +0300

    Add project files.

---
+++ EntityLayer/Concrete/Content.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+   public class Content
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [StringLength(1000)]
+        public string Value { get; set; }
+        public DateTime Date { get; set; }
+
+
+        public virtual Tittle Tittle { get; set; }
+        public int TittleID { get; set; }
+
+        public virtual Writer Writer { get; set; }
+        public int? WriterID { get; set; }
+    }
+}
