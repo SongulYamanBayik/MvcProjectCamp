678ad8854cc1ea08df4615851f9c23293ed7a1b5
Songül YAMAN BAYIK <snglymn@gmail.com>
2022-10-25 17:08:25 +0300

    template created

---
+++ BusinessLayer/ValidationRules/CategoryValidator.cs
@@ -0,0 +1,21 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+   public class CategoryValidator: AbstractValidator<Category>
+    {
+        public CategoryValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Kategori İsmi Boş Geçilemez.");
+            RuleFor(x => x.Description).NotEmpty().WithMessage("Açıklama Alanı Boş Geçilemez.");
+            RuleFor(x => x.Name).MinimumLength(5).WithMessage("Kategori İsmi En Az 5 Karakter Olmalıdır.");
+            RuleFor(x => x.Name).MaximumLength(50).WithMessage("Kategori İsmi En Fazla 50 Karakter Olmalıdır.");
+        }
+    }
+}
--- MvcProjectCamp/Controllers/CategoryController.cs
+++ MvcProjectCamp/Controllers/CategoryController.cs
@@ -1,4 +1,8 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DAL.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,12 +13,38 @@ namespace MvcProjectCamp.Controllers
 {
     public class CategoryController : Controller
     {
-        CategoryManager categoryManager = new CategoryManager();
+        CategoryManager categoryManager = new CategoryManager(new EFCategoryDal());
         // GET: Category
         public ActionResult Index()
         {
-            var value = categoryManager.GetAllBL();
+            var value = categoryManager.TList();
             return View(value);
+        }
+
+        [HttpGet]
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddCategory(Category category)
+        {
+            CategoryValidator categoryValidator = new CategoryValidator();
+            ValidationResult validationResult = categoryValidator.Validate(category);
+            if (validationResult.IsValid)
+            {
+                categoryManager.TInsert(category);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in validationResult.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage); 
+                }
+            }
+            return View();
         }
     }
 }
